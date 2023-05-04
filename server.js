9470a9395c40d42451f6116068006aea035d61ed
DJ Neill <dj.neill856@gmail.com>
2023-05-04 03:08:17 -0500

    pushing star wars quotes crud app

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+// ========================
+// Link to Database
+// ========================
+const connectionString = process.env.DB_URL
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+        app.set('view engine', 'ejs')
+
+        // ========================
+        // Middlewares
+        // ========================
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
+
+        // ========================
+        // Routes
+        // ========================
+        app.get('/', (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                { name: 'Yoda' },
+                {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote,
+                    },
+                },
+                {
+                    upsert: true,
+                }
+            )
+                .then(result => {
+                    res.json('Sucess')
+                })
+        })
+
+        app.delete('/quotes', (req, res) => {
+            quotesCollection.deleteOne({ name: req.body.name })
+                .then(result => {
+                    if (result.deletedCount === 0) {
+                        return res.json('No quote to delete')
+                    }
+                    res.json(`Deleted Darth Vader's quote`)
+                })
+                .catch(error => console.error(error))
+        })
+
+        // ========================
+        // Listen
+        // ========================
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        })
+    })
+    .catch(error => console.error(error))
+
+console.log('May Node be with you')
